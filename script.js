9584964ab08602b6a40f7e4ddc9d96784fc8f410
Jackie Tan <jackilyn.tan@gmail.com>
2023-02-26 20:14:45 -0500

    butchered

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Assignment code here
 
+var result = document.querySelector("result");
+var length = document.querySelector("length");
+var lengthResult = document.querySelector("lengthResult");
+var numbers = document.querySelector("numbers");
+var characters = document.querySelector("characters");
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,6 +19,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  // var length = range 8-128
+  var lower = ["abcdefghijklmnopqrstuvwxyz"];
+  var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var special = ["~!@#$%^&*()_+=-`|{}:<>?/."];
+  var numbers = ["1234567890"];
 
 }
 
