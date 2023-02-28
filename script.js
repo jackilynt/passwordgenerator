67bde04c1b940bc0efc1d6f975a3ce2aa39e350d
Jackie Tan <jackilyn.tan@gmail.com>
2023-02-27 22:21:31 -0500

    butcheredcode

--- script.js
+++ script.js
@@ -97,6 +97,19 @@ function generatePassword() {
   return randomPassword;
 }
 
+var password=document.getElementById("password");
+
+ function generatePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 8;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
   
