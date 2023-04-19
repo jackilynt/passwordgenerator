00c6d0c89d0f9a2a4a966de58d97ecb87449249c
Jackie Tan <jackilyn.tan@gmail.com>
2023-04-19 09:19:54 -0400

    redo

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Assignment Code
+
 var generateBtn = document.querySelector('#generate');
 
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var special = [  "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"];
-var numbers = new Array(1,2,3,4,5,6,7,8,9,0);
-
+var special = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"];
+var numbers = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
 
 // Write password to the #password input
 function writePassword() {
@@ -22,30 +21,28 @@ function generatePassword() {
   var passwordLength = 0;
 
   var length = prompt("How many characters are in your password minimum 8 maximum 128?");
-    if (length < 8 || length > 128) {
+  if (length < 8 || length > 128) {
     alert("Password does not meet minimum requirements. Start Over.");
     return null;
   }
 
-  if (length === undefined) {
-    alert("Password does not meet minimum requirements. Start Over.");
-    return "The Password";
-  }
+  passwordLength = length;
 
   var pwdUpperCase = confirm("Include upper case?");
   var pwdLowerCase = confirm("Include lower case?");
   var pwdNumbers = confirm("Include numbers?");
   var pwdSpecial = confirm("Include symbols?");
 
-  if(!upper && !lower && !numbers && !special){
+  if (!pwdUpperCase && !pwdLowerCase && !pwdNumbers && !pwdSpecial) {
     alert("Password does not meet minimum requirements. Start Over.");
     return null;
   }
-  if (upper) {
-    passwordOptions = passwordOptions.concat(pwdUpperCase);
+
+  if (pwdUpperCase) {
+    passwordOptions = passwordOptions.concat(upper);
   }
-  if (lower) {
-    passwordOptions = passwordOptions.concat(pwdLowerCase);
+  if (pwdLowerCase) {
+    passwordOptions = passwordOptions.concat(lower);
   }
   if (numbers) {
     passwordOptions = passwordOptions.concat(pwdNumbers);
@@ -67,4 +64,3 @@ function generatePassword() {
 }
 
 generateBtn.addEventListener("click", writePassword);
-
